57b6534e45fb1fc37b79a5f69dd2f154d61e7031
andresmmera <andresmartinezmera@gmail.com>
2016-01-26 21:14:13 +0100

    Some CAPQ/INDQ documentation
    Added short description of the CAPQ/INDQ models to the technical papers

--- qucs/qucs/components/indq.cpp
+++ qucs/qucs/components/indq.cpp
@@ -66,7 +66,7 @@ indq::indq()
   Props.append(new Property("f", "100 MHz", true,
 		QObject::tr("Frequency at which Q is measured")));
   Props.append(new Property("Mode", "Linear", true,
-		QObject::tr("Frequency dependence mode of Q")+
+		QObject::tr("Q frequency profile")+
 		" [Linear, Quadratic, Constant]"));
 }
 indq::~indq()
